5ea793c35a572658e4724ed119823355a692b4cb
Soyka <petr.emelianov@icloud.com>
2023-03-06 19:23:48 +0200

    visual part

--- main.py
+++ main.py
@@ -8,5 +8,14 @@ import pyaudio
 
 class VoiceRecorder():
 
+    def __init__(self):
+        self.root = tkinter.Tk()
+        self.root.resizable(True, False)
+        self.button = tkinter.Button(
+            text="🎙️",
+            font=('Arial', 120, "bold")
+        )
+        self.button.pack()
+        self.root.mainloop()
 
 VoiceRecorder()
